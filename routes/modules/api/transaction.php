1f4a44a74d87d3294031e210012cb948b1503cd1
andrey <rsemscom@yandex.ru>
2021-03-02 16:12:46 +0200

    csrf remove

--- routes/modules/api/transaction.php
+++ routes/modules/api/transaction.php
@@ -3,18 +3,19 @@
 use App\Models\ProjectRoad;
 use App\Models\Project;
 
-Route::post('/api/transaction', function () {
+Route::post('/api/transaction', function (Request $request) {
 
     $result = [
       'success' => true
     ];
+
+    var_dump($request->all());
 
     $map = [];
 
     global $currentProject;
     $map[$currentProject->id] = $currentProject;
 
-    var_dump($_POST, $currentProject);
 
     return json_encode($result);
 });
