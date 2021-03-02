5498e92d6238ffead4af169e2beee92ffa41b5fb
andrey <rsemscom@yandex.ru>
2021-03-02 16:40:33 +0200

    transaction fix

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,6 +7,18 @@ use Illuminate\Notifications\Notifiable;
 
 class Transaction extends Model
 {
+    static function statuses() {
+        return [
+            1 => 'Ожидает',
+            2 => 'Открыта',
+            3 => 'Успешна',
+            4 => 'Ошибка',
+        ];
+    }
+
+
+    const DIRECT = 1;
+
     protected $table = 'transaction';
 
     /**
@@ -15,15 +27,24 @@ class Transaction extends Model
      * @var array
      */
     protected $fillable = [
-        'tid', 'nextDate', 'retryCount', 'errorCode', 'status', 'amount', 'currency', 'data'
+        'tid', 'type', 'nextDate', 'retryCount', 'errorCode', 'status', 'amount', 'currency', 'data'
     ];
 
     public $timestamps = false;
 
 
+    public static function generateTid() {
+
+        $t = dechex(time()).rand(1000000, 9999999).rand(1000000, 9999999).rand(1000000, 9999999).rand(1000000, 9999999).rand(100, 999);
+
+
+        return $t;
+    }
+
     public static function getListFields() {
         return [
             'id' => 'ID',
+            'type' => 'Тип',
             'tid' => 'ID транзакции',
             'nextDate' => 'Время обновления',
             'retryCount' => 'Кол-во ретраев',
@@ -48,6 +69,14 @@ class Transaction extends Model
     }
 
     public function getAttr($attr) {
+        if ($attr === 'type') {
+            return $this->type === '1' ? 'Прямая транзакция' : 'Неизвестно';
+        }
+
+        if ($attr === 'status') {
+            return self::statuses()[$this->status];
+        }
+
         return $this->$attr;
     }
 }
--- routes/modules/api/transaction.php
+++ routes/modules/api/transaction.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Models\ProjectRoad;
-use App\Models\Project;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 Route::post('/api/transaction', function (Request $request) {
@@ -10,12 +9,40 @@ Route::post('/api/transaction', function (Request $request) {
       'success' => true
     ];
 
-    var_dump($request->all());
+    $all = $request->all();
 
-    $map = [];
+    $amount = 0;
+    $code = 0;
 
-    global $currentProject;
-    $map[$currentProject->id] = $currentProject;
+    if (!$all['amount'] || !$all['from'] || !$all['to'] || !$all['pkey']) {
+        $code = 1001;
+
+        $status = 4;
+    } else {
+        $amount = +$all['amount'];
+
+        $code = 1002;
+
+        $status = $code === 0;
+    }
+
+    $result = array_merge($result, $all);
+
+    $t = new Transaction();
+
+    $t->type = Transaction::DIRECT;
+    $t->tid = Transaction::generateTid();
+    $t->nextDate = '2999-01-01 00:00:00';
+    $t->retryCount = '0';
+    $t->errorCode = $code;
+    $t->status = 3;
+    $t->amount = $amount;
+    $t->currency = 'DLXT';
+    $t->data = json_encode($result);
+
+    $t->save();
+
+
 
 
     return json_encode($result);
