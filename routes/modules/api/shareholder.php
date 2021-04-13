f7081207762f48a17d833242bc5848b687d39488
andrey <rsemscom@yandex.ru>
2021-04-13 13:56:44 +0300

    shareholder fix

---
+++ routes/modules/api/shareholder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\Shareholder;
+use \App\Http\Helpers\ApiHelper;
+use Illuminate\Http\Request;
+
+Route::get('/api/shareholder', function (Request $request) {
+
+    global $currentProject;
+
+    if ($currentProject->status === 2) {
+        return [
+            'success' => false,
+            'error_code' => 1011,
+            'error' => 'Project Blocked'
+        ];
+    }
+
+    if ($errors = ApiHelper::checkAttributes([
+        'key' => [],
+    ], $request)) {
+        return [
+            'success' => false,
+            'error_code' => 1522,
+            'errors' => $errors
+        ];
+    }
+
+    $tid = $_GET['transaction'] ?? NULL;
+    $user_id = $_GET['uid'] ?? NULL;
+
+    if (!$tid && !$user_id) {
+        return [
+            'success' => false,
+            'error_code' => 1522,
+            'errors' => [
+                'error' => 'uid or telegram_id should be set'
+            ]
+        ];
+    }
+
+    $sh = ($tid ? Shareholder::where('telegram', 'like', '%'.$tid.'%') : Shareholder::where('uid', 'like', $user_id))->first();
+
+    $result = [
+        'success' => true,
+        'telgram_id' => $sh
+    ];
+
+
+    return json_encode($result);
+});
