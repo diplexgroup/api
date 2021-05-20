5827c8993778bcaab58dcca2f9d39905ca70ada8
Anton Karasev <uselessfire@gmail.com>
2021-05-21 02:54:49 +0300

    JS for fee stategy

--- public/js/main.js
+++ public/js/main.js
@@ -23,22 +23,25 @@ jQuery(function ($) {
 
 
         var fee_strategy_tr = $('#fee-strategy-tr');
-        fee_strategy_tr.hide();
+        // fee_strategy_tr.hide();
 
         $('<tr><td>Фиксированная сумма комиссии: </td><td><input type="number" value="" id="amount"></td></tr>').insertAfter(fee_strategy_tr);
         $('<tr><td>Процент комиссии</td><td><input type="number" min="0" max="1" step="0.0001" id="percent"></td></tr>').insertAfter(fee_strategy_tr);
         $('#percent').val(JSON.parse(el.val())['percent']);
         $('#amount').val(JSON.parse(el.val())['amount']);
+        var x = fee_strategy_tr.detach();
+        x.appendTo('.content form');
+        x.hide();
 
 
         $('#amount, #percent').on('change keypress input', changeJSON);
     }
 
     // script for /project_roads/view
-    if (window.location.pathname.startsWith('/project_roads/view')) {
+    else if (window.location.pathname.startsWith('/project_roads/view')) {
 
-        var el = $('tr:contains("Стратегия категорий")');
-        el.hide();
+        // var el = $('tr:contains("Стратегия категорий")');
+        var el = $('#tax_strategy');
         var content = JSON.parse(el.contents()[3].innerHTML);
         if (content["percent"]) {
             $(`<tr><td>Процент комиссии</td><td>${content["percent"]}</td></tr>`).insertAfter(el);
@@ -46,6 +49,8 @@ jQuery(function ($) {
         if (content["amount"]) {
             $(`<tr><td>Фиксированная сумма комиссии</td><td>${content["amount"]}</td></tr>`).insertAfter(el);
         }
+        //el.hide();
+        el.remove();
     }
 
 });
