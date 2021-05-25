67c9797d200f4805fe295464d21369a17bc0b3b7
Anton Karasev <uselessfire@gmail.com>
2021-05-25 21:44:35 +0300

    "Send coins" page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -192,6 +192,14 @@
                             </p>
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a href="/send-wallet-wallet" class="nav-link">
+                            <i class="nav-icon fas fa-th"></i>
+                            <p>
+                                Перевод монет
+                            </p>
+                        </a>
+                    </li>
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
